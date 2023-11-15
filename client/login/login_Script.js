186180c7f4ff4fa0baec93090ac7b82e6bcd0cf0
Dimitris Stasinos <staa.dista@yahoo.gr>
2023-11-15 20:41:45 +0200

    ok

--- client/login/login_Script.js
+++ client/login/login_Script.js
@@ -1,9 +1,22 @@
-function showPassword() {
-  var inputPass = document.getElementById("pass");
+function submitForm() {
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
 
-  if(inputPass.type === "password") {
-    inputPass.type = "text";
-  }else {
-    inputPass.type = "password";
-  }
+  // You can perform validation here before sending the data to the server
+
+  const xhr = new XMLHttpRequest();
+  xhr.open('POST', '/login', true);
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+          const data = JSON.parse(xhr.responseText);
+          if (data.success) {
+              window.location.href = '/dashboard'; // Redirect to the dashboard on successful login
+          } else {
+              document.getElementById('error-message').innerText = data.message;
+          }
+      }
+  };
+  const requestBody = JSON.stringify({ username, password });
+  xhr.send(requestBody);
 }
--- server/backend.js
+++ server/backend.js
@@ -1,9 +1,43 @@
-var http = require('http');
-var fs = require('fs');
-http.createServer(function (req, res) {
-  fs.readFile('../client/login/login_pageV2.html', function(err, data) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
-  });
-}).listen(8080);
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+    if (req.method === 'GET' && req.url === '/') {
+        // Serve the HTML file
+        fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('Internal Server Error');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(data);
+            }
+        });
+    } else if (req.method === 'POST' && req.url === '/login') {
+        // Handle login logic
+        let body = '';
+        req.on('data', chunk => {
+            body += chunk;
+        });
+        req.on('end', () => {
+            const { username, password } = JSON.parse(body);
+            // Simple authentication logic (replace with a secure implementation)
+            if (username === 'user' && password === 'password') {
+                res.writeHead(200, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify({ success: true }));
+            } else {
+                res.writeHead(200, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify({ success: false, message: 'Invalid username or password' }));
+            }
+        });
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Not Found');
+    }
+});
+
+const port = 3000;
+server.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
