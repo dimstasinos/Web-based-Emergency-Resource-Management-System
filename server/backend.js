a13ba8b6227af845c8d99c8256674e8626ac817a
Dimitris Stasinos <staa.dista@yahoo.gr>
2023-11-17 16:30:41 +0200

    ok

--- server/backend.js
+++ server/backend.js
@@ -1,43 +1,8 @@
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
+const http = require("http");
+const host = 'localhost';
+const port = 8000;
 
-const server = http.createServer((req, res) => {
-    if (req.method === 'GET' && req.url === '/') {
-        // Serve the HTML file
-        fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
-            if (err) {
-                res.writeHead(500, { 'Content-Type': 'text/plain' });
-                res.end('Internal Server Error');
-            } else {
-                res.writeHead(200, { 'Content-Type': 'text/html' });
-                res.end(data);
-            }
-        });
-    } else if (req.method === 'POST' && req.url === '/login') {
-        // Handle login logic
-        let body = '';
-        req.on('data', chunk => {
-            body += chunk;
-        });
-        req.on('end', () => {
-            const { username, password } = JSON.parse(body);
-            // Simple authentication logic (replace with a secure implementation)
-            if (username === 'user' && password === 'password') {
-                res.writeHead(200, { 'Content-Type': 'application/json' });
-                res.end(JSON.stringify({ success: true }));
-            } else {
-                res.writeHead(200, { 'Content-Type': 'application/json' });
-                res.end(JSON.stringify({ success: false, message: 'Invalid username or password' }));
-            }
-        });
-    } else {
-        res.writeHead(404, { 'Content-Type': 'text/plain' });
-        res.end('Not Found');
-    }
-});
-
-const port = 3000;
-server.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
-});
+const requestListener = fuction (req, res) {
+  res.writeHead(200);
+  res.end("My first server!");
+}
