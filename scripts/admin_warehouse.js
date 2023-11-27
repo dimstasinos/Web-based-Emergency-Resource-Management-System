a98efdac37001112e5e0046fff7411a570d075b1
Dimitris Stasinos <staa.dista@yahoo.gr>
2023-11-27 20:31:11 +0200

    οκ

--- scripts/admin_warehouse.js
+++ scripts/admin_warehouse.js
@@ -1,18 +1,22 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // Fetch JSON data
-  fetch('/export.json')
-    .then(response => response.json())
-    .then(data => {
-    
-      
+document.addEventListener('DOMContentLoaded', function () {
+  var fetchButton = document.getElementById('fetchButton');
+  var resultDiv = document.getElementById('result');
 
-    })
-    .catch(error => console.error('Error fetching data:', error));
+  fetchButton.addEventListener('click', function () {
+      // Make an AJAX request to the PHP script
+      var xhr = new XMLHttpRequest();
+      xhr.onreadystatechange = function () {
+          if (xhr.readyState == 4 && xhr.status == 200) {
+              // Parse the received JSON data
+              var jsonData = JSON.parse(xhr.responseText);
+
+              // Display the JSON data in the result div
+              resultDiv.innerHTML = '<pre>' + JSON.stringify(jsonData, null, 2) + '</pre>';
+          }
+      };
+
+      // Replace 'getData.php' with the actual path to your PHP script
+      xhr.open('GET', '/server/warehouse.php', true);
+      xhr.send();
+  });
 });
-
-
-function test() {
-
-  
-
-};
--- server/warehouse.php
+++ server/warehouse.php
@@ -1,6 +1,7 @@
 <?php
   $web_storage = file_get_contents("http://usidas.ceid.upatras.gr/web/2023/export.php");
 
+  header('Content-Type: application/json');
   echo $web_storage;
-
+  
 ?>
