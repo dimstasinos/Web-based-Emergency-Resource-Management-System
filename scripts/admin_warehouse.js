8ec88e75b874b4c48e4d6a61a60973613f55604a
Dimitris Stasinos <staa.dista@yahoo.gr>
2023-11-30 22:14:14 +0200

    ajax

--- scripts/admin_warehouse.js
+++ scripts/admin_warehouse.js
@@ -11,9 +11,16 @@ document.getElementById('online_data').addEventListener('click', function () {
 
 function categories_select(data) {
 
-    const list = document.getElementById("cat");
 
-    for(let )
+
+  const list = document.getElementById("cat");
+  
+  data.categories.forEach(category => {
+    let select_add = document.createElement("option");
+    select_add.textContent = category.category_name;
+    list.appendChild(select_add);
+  });
+    
   
 }
 
