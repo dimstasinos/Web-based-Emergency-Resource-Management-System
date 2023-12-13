0c9c703050cb6bf1ca2db90be24c8ca7a3256aec
Dimitris Stasinos <staa.dista@yahoo.gr>
2023-12-13 15:52:43 +0200

    ok

--- server/warehouse_admin/delete_category.php
+++ server/warehouse_admin/delete_category.php
@@ -11,11 +11,17 @@ try {
   $delete_stmt = $db->prepare("DELETE FROM item_category WHERE
   category_id=?");
 
+  $delete_stmt->bind_param(
+    "i",
+    $data->id
+  );
 
+  $delete_stmt->execute();
+  $db->close();
 
+  header('Content-Type: application/json');
+  echo json_encode(['status' => 'success']);
 } catch (Exception $error) {
   header('Content-Type: application/json');
   echo json_encode(['status' => 'error', "Error: " . $error->getMessage()]);
 }
-
-?>
