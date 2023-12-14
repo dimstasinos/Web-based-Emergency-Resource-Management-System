6657203ea1b4d9bca6f180cc8635e0fcdd0a90c7
VasilisPistiolas <83075825+VasilisPistiolas@users.noreply.github.com>
2023-12-14 15:38:59 +0200

    ok

--- server/Mysql_connection.php
+++ server/Mysql_connection.php
@@ -6,7 +6,11 @@ function db_connect()
 
   $name = "localhost";
   $user = "root";
+<<<<<<< HEAD
   $password = "7458";
+=======
+  $password = "3003";
+>>>>>>> 0bef056781a0c75b53246150956b1ebdc53871b9
   $database_name = "project";
 
   $db = new mysqli($name, $user, $password, $database_name);
