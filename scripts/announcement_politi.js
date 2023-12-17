f0e988fa204de756a87cfad5f6c1596b1f3f1d9c
VasilisPistiolas <83075825+VasilisPistiolas@users.noreply.github.com>
2023-12-17 17:53:42 +0200

    ok

---
+++ scripts/announcement_politi.js
@@ -0,0 +1,45 @@
+var onload_data;
+
+document.addEventListener('DOMContentLoaded', function () {
+
+  fetch('/server/announcement/announcement.php')
+    .then(response => response.json())
+    .then(data => {
+      if (data.status === "error") {
+        console.error("Server Error:", data.Error);
+      } else {
+        const tableBody = document.getElementById('table_announcement');
+
+        data.announcements.forEach(item => {
+
+          const row = document.createElement('tr');
+
+
+          const idCell = document.createElement('td');
+          idCell.textContent = item.id;
+          row.appendChild(idCell);
+
+          const textCell = document.createElement('td');
+          textCell.textContent = item.text;
+          row.appendChild(textCell);
+
+          const dateCell = document.createElement('td');
+          dateCell.textContent = item.date;
+          row.appendChild(dateCell);
+
+          const quantityCell = document.createElement('td');
+          quantityCell.textContent = item.quantity;
+          row.appendChild(quantityCell);
+
+
+          tableBody.appendChild(row);
+
+
+
+        });
+      }
+
+    })
+    .catch(error => console.error('Error fetching data:', error));
+
+});
