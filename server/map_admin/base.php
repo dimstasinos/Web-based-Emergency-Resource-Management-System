6fbca22cd17ca26b4100b399cc7da6abf3cbbd23
Dimitris Stasinos <staa.dista@yahoo.gr>
2023-12-17 16:30:09 +0200

    ok

---
+++ server/map_admin/base.php
@@ -0,0 +1,36 @@
+<?php
+
+include("../Mysql_connection.php");
+
+try {
+  $db = db_connect();
+
+  $mysql = "SELECT * from base";
+  $response = $db->query($mysql);
+
+  if ($response->num_rows > 0) {
+
+    while ($row = $response->fetch_assoc()) {
+      $base_array = array(
+        "lat" => $row["lat"],
+        "longi" => $row["longi"],
+
+      );
+    }
+  }
+
+  $base[] = $base_array;
+
+  $data = array(
+    "base_location" => $base,
+  );
+
+  $db->close();
+
+  $json_data = json_encode($data);
+  header('Content-Type: application/json');
+  echo $json_data;
+} catch (Exception $error) {
+  header('Content-Type: application/json');
+  echo json_encode(['status' => 'error', "Error" => $error->getMessage()]);
+}
