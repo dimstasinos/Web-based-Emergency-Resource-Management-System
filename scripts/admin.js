d8de5800c1dd4adaac0c41fb377fd65d2796c439
varvarakar <150253895+varvarakar@users.noreply.github.com>
2023-12-17 14:21:05 +0200

    kkk

--- scripts/admin.js
+++ scripts/admin.js
@@ -6,24 +6,45 @@ document.addEventListener('DOMContentLoaded', function () {
   }).addTo(map);
 
 
-  fetch('/server/map_admin/location.php')
+  fetch('/server/location.php')
     .then(response => response.json())
     .then(data => {
       console.log(data);
-      createAndPinMarker(data);
 
     })
     .catch(error => console.error('Error:', error));
+  });
 
 
-});
-   fetch('/server/map_admin/requests.php')
-   .then(response => response.json())
-   .then(data => {
-     console.log(data);
- 
-   })
-   .catch(error => console.error('Error:', error));
+     fetch('/server/map_admin/requests.php')
+     .then(response => response.json())
+     .then(data => {
+       console.log(data);
+  
+       var map = L.map('map').setView([37.9838, 23.7275], 13);
+
+       var requestsData = data.requests;
+  
+       for (var i = 0; i < requestsData.length; i++) {
+         var request = requestsData[i];
+  
+         var requestMarker = L.marker([request.lat, request.longi], {
+         }).addTo(map);
+
+         var popupContent = `<b>Name:</b> ${request.citizen_name}<br>
+                             <b>Phone:</b> ${request.phone_number}<br>
+                             <b>Date:</b> ${request.submission_date}<br>
+                             <b>Type:</b> ${request.request_type}<br>
+                             <b>Quantity:</b> ${request.quantity}<br>
+                             <b>Pickup Date:</b> ${request.pickup_date}<br>
+                             <b>Vehicle Username:</b> ${request.veh_username}`;
+
+         requestMarker.bindPopup(popupContent);
+       }
+     })
+     .catch(error => console.error('Error:', error));
+  
+  
 
 
 
@@ -58,56 +79,39 @@ function createAndPinMarker(data) {
     var lat = data.lat;
     var long = data.longi;
     var typeloc = data.typeloc;
-
-    // Customize marker based on type
     var markerColor;
     var popupContent;
 }
+if (typeloc === 'off') {
+  markerColor = 'green';
+  popupContent = '<b>Base</b><br>Base Location';
+} else if (typeloc === 'veh') {
+  markerColor = 'blue';
+  popupContent = 'Username: ' + data.username + '<br>Load: ' + data.load +
+    '<br>Condition: ' + data.condition;
 
-    switch (typeloc) {
-      case 'off':
-        markerColor = 'green';
-        popupContent = '<b>Base</b><br>Base Location';
-        break;
+  connectMarkers([lat, long], tasksMarkers[data.username]);
+} else if (typeloc === 'req') {
+  markerColor = 'red';
+  popupContent = 'Username: ' + data.username + '<br>Load: ' + data.load +
+    '<br>Condition: ' + data.condition;
+} else {
+  console.error('Unknown marker type:', typeloc);
+  return;
+}
 
-        case 'veh':
-        markerColor = 'blue';
-        popupContent = 'Username: ' + data.username + '<br>Load: ' + data.load +
-          '<br>Condition: ' + data.condition;
-
-          connectMarkers([lat, long], tasksMarkers[data.username]);
-        break;
-      
-        case 'req':
-        markerColor = 'red';
-        popupContent = 'Username: ' + data.username + '<br>Load: ' + data.load +
-          '<br>Condition: ' + data.condition;
-
-      default:
-        console.error('Unknown marker type:', typeloc);
-        return;
-    }
-
-    // Create and add the marker to the map
-    var marker = L.marker([lat, long], { icon: L.divIcon({ className: 'map-marker', html: markerColor }) }).addTo(map);
-    marker.bindPopup(popupContent).openPopup();
-
-    // Store the marker for future reference
-    switch (typeloc) {
-      case 'veh':
-        vehiclesMarkers[data.username] = marker;
-        break;
-      case 'req':
-        requestsMarkers[data.id] = marker;
-        break;
-      case 'off':
-        offersMarkers[data.id] = marker;
-        break;
-    }
-  }
+var marker = L.marker([lat, long], { icon: L.divIcon({ className: 'map-marker', html: markerColor }) }).addTo(map);
+marker.bindPopup(popupContent).openPopup();
 
 
-// Connect two markers with a straight line
+if (typeloc === 'veh') {
+  vehiclesMarkers[data.username] = marker;
+} else if (typeloc === 'req') {
+  requestsMarkers[data.id] = marker;
+} else if (typeloc === 'off') {
+  offersMarkers[data.id] = marker;
+}
+
 function connectMarkers(coords1, coords2) {
 if (coords1 && coords2) {
   var line = L.polyline([coords1, coords2.getLatLng()], { color: 'black' }).addTo(map);
