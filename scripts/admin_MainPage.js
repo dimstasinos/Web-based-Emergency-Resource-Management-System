63876cecf18c0e707c98a75a9c7f51b00f431f3b
Dimitris Stasinos <staa.dista@yahoo.gr>
2023-12-22 17:18:54 +0200

    ok

--- scripts/admin_MainPage.js
+++ scripts/admin_MainPage.js
@@ -46,6 +46,20 @@ document.addEventListener('DOMContentLoaded', function () {
                 iconAnchor: [15, 15],
                 popupAnchor: [0, -15]
               });
+            }else if (category === "Request Accepted") {
+              return L.icon({
+                iconUrl: '/leaflet/images/offer-green.png',
+                iconSize: [30, 30],
+                iconAnchor: [15, 15],
+                popupAnchor: [0, -15]
+              });
+            }else if (category === "Request Pending") {
+              return L.icon({
+                iconUrl: '/leaflet/images/request-red.png',
+                iconSize: [30, 30],
+                iconAnchor: [15, 15],
+                popupAnchor: [0, -15]
+              });
             }
           })(),
           draggable: category === "Base"
@@ -99,7 +113,7 @@ document.addEventListener('DOMContentLoaded', function () {
           });
         } else if (category === "Request Pending") {
 
-          var info_citizen = `<div style="max-height: 200px; overflow-y: auto;">
+          var info_citizen = `<div style="max-height: 150px; overflow-y: auto;">
           <strong>Citizen</strong><br>
           <strong>Name:</strong> ${feature.properties.first_name} ${feature.properties.last_name}<br>
           <strong>Phone number:</strong> ${feature.properties.phone_number}<br>
@@ -121,7 +135,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
         } else if (category === "Request Accepted") {
 
-          var info_citizen = `<div style="max-height: 200px; overflow-y: auto;">
+          var info_citizen = `<div style="max-height: 150px; overflow-y: auto;">
           <strong>Citizen</strong><br>
           <strong>Name:</strong> ${feature.properties.first_name} ${feature.properties.last_name}<br>
           <strong>Phone number:</strong> ${feature.properties.phone_number}<br>
@@ -140,6 +154,27 @@ document.addEventListener('DOMContentLoaded', function () {
 
           info_citizen = info_citizen + `</div>`;
           customMarkers.bindPopup(info_citizen);
+        } else if (category === "Offer Accepted") {
+          var info_citizen = `<div style="max-height: 150px; overflow-y: auto;">
+          <strong>Citizen</strong><br>
+          <strong>Name:</strong> ${feature.properties.first_name} ${feature.properties.last_name}<br>
+          <strong>Phone number:</strong> ${feature.properties.phone_number}<br>
+          ----------------------------------`;
+
+          feature.properties.details.forEach((offer) => {
+            info = `<br><strong>Request</strong><br>
+            <strong>Submission date:</strong> ${offer.submission_date}<br>
+            <strong>Item:</strong> ${offer.item_name}<br>
+            <strong>Quantity:</strong> ${offer.quantity}<br>
+            <strong>Pickup date:</strong> ${offer.pickup_date}<br>
+            <strong>Vehicle username:</strong> ${offer.vehicle_username}<br>
+            ----------------------------------`;
+            info_citizen = info_citizen + info;
+          });
+
+          info_citizen = info_citizen + `</div>`;
+          customMarkers.bindPopup(info_citizen);
+
         }
 
 
--- server/map_admin/map.php
+++ server/map_admin/map.php
@@ -9,7 +9,7 @@ $features = array();
 $mysql = "SELECT DISTINCT req_citizen_id FROM citizen_requests;";
 $response = $db->query($mysql);
 
-$check;
+$check = 0;
 
 if ($response->num_rows > 0) {
   while ($requests_row = $response->fetch_assoc()) {
@@ -29,7 +29,7 @@ if ($response->num_rows > 0) {
     $location["longitude"] = $citizen_row["longitude"];
 
     $coordinates = array((float)$location["latitude"], (float)$location["longitude"]);
-    $gemetry = array(
+    $geometry = array(
       "type" => "Point",
       "coordinates" => $coordinates
     );
@@ -65,7 +65,7 @@ if ($response->num_rows > 0) {
         $vehicle_rensponse = $mysql->get_result();
         $vehicle_row = $vehicle_rensponse->fetch_assoc();
         $request_array["vehicle_username"] = $vehicle_row["vehicle_username"];
-      }else{
+      } else {
         $request_array["vehicle_username"] = null;
       }
 
@@ -90,7 +90,7 @@ if ($response->num_rows > 0) {
 
     $feature = array(
       "type" => "Feature",
-      "geometry" => $gemetry,
+      "geometry" => $geometry,
       "properties" => $properties,
     );
     $features[] = $feature;
@@ -98,10 +98,110 @@ if ($response->num_rows > 0) {
 }
 
 
+$mysql = "SELECT DISTINCT offer_citizen_id FROM citizen_offers;";
+$response = $db->query($mysql);
+
+$check = 0;
 
 
+if ($response->num_rows > 0) {
+  while ($offer_row = $response->fetch_assoc()) {
+
+    $check = 0;
+
+    $mysql = $db->prepare("SELECT f_name,l_name,phone_number,latitude,longitude FROM citizen
+    where citizen_id=?");
+    $mysql->bind_param("i", $offer_row["offer_citizen_id"]);
+    $mysql->execute();
+    $citizen_rensponse = $mysql->get_result();
+    $citizen_row = $citizen_rensponse->fetch_assoc();
+
+    $citizen_data["first_name"] = $citizen_row["f_name"];
+    $citizen_data["last_name"] = $citizen_row["l_name"];
+    $citizen_data["phone_number"] = $citizen_row["phone_number"];
+    $location["latitude"] = $citizen_row["latitude"];
+    $location["longitude"] = $citizen_row["longitude"];
+
+    $coordinates = array((float)$location["latitude"], (float)$location["longitude"]);
+    $geometry = array(
+      "type" => "Point",
+      "coordinates" => $coordinates
+    );
+
+    $mysql = $db->prepare("SELECT * FROM citizen_offers where offer_citizen_id=?");
+    $mysql->bind_param("i", $offer_row["offer_citizen_id"]);
+    $mysql->execute();
+    $citizen_rensponse = $mysql->get_result();
+    $offer_details = array();
+
+    while ($citizen_row = $citizen_rensponse->fetch_assoc()) {
+
+      $offer_array["offer_id"] = $citizen_row["offer_id"];
+      $offer_array["submission_date"] = $citizen_row["submission_date"];
+      $offer_array["pickup_date"] = $citizen_row["pickup_date"];
+      $offer_array["vehicle_id"] = $citizen_row["offer_veh_id"];
+      $offer_array["citizen_id"] = $citizen_row["offer_citizen_id"];
 
 
+      $mysql = $db->prepare("SELECT * FROM offer_items where offer_id_item=?");
+      $mysql->bind_param("i", $offer_array["offer_id"]);
+      $mysql->execute();
+      $item_rensponse = $mysql->get_result();
+      $items_info = array();
+      while ($item_row = $item_rensponse->fetch_assoc()) {
+
+        $item_details["item_id"] = $item_row["item_id_offer"];
+        $item_details["quantity"] = $item_row["quantity"];
+
+
+        $mysql = $db->prepare("SELECT item_name FROM items where item_id=?");
+        $mysql->bind_param("i", $item_details["item_id"]);
+        $mysql->execute();
+        $item_rensponse_name = $mysql->get_result();
+        $item_name_row = $item_rensponse_name->fetch_assoc();
+        $item_details["item_name"] = $item_name_row["item_name"];
+        
+        $items_info[] = $item_details;
+      }
+
+
+      if ($citizen_row["offer_veh_id"] != null) {
+        $mysql = $db->prepare("SELECT vehicle_username FROM vehicle where vehicle_id=?");
+        $mysql->bind_param("i", $offer_array["vehicle_id"]);
+        $mysql->execute();
+        $vehicle_rensponse = $mysql->get_result();
+        $vehicle_row = $vehicle_rensponse->fetch_assoc();
+        $offer_array["vehicle_username"] = $vehicle_row["vehicle_username"];
+      } else {
+        $offer_array["vehicle_username"] = null;
+      }
+
+      $offer_array["items"] = $items_info;
+
+      if ($citizen_row["offer_veh_id"] == null) {
+        $check = 1;
+      }
+
+      $offer_details[] = $offer_array;
+    }
+
+    if ($check == 1) {
+      $citizen_data["category"] = "Offer Pending";
+    } else {
+      $citizen_data["category"] = "Offer Accepted";
+    }
+
+    $citizen_data["details"] = $offer_details;
+    $properties = $citizen_data;
+
+    $feature = array(
+      "type" => "Feature",
+      "geometry" => $geometry,
+      "properties" => $properties,
+    );
+    $features[] = $feature;
+  }
+}
 
 
 
