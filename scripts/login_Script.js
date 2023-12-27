92ba44b020c308ec32d41930bcc292ec37f1f344
Dimitris Stasinos <staa.dista@yahoo.gr>
2023-12-27 16:18:49 +0200

    ok

--- scripts/login_Script.js
+++ scripts/login_Script.js
@@ -1,30 +1,3 @@
-/*document.getElementById('loginButton').addEventListener('click', function () {
-  username = document.getElementById("username").value;
-  password = document.getElementById("password").value;
-
-  const data = {
-    username: username,
-    password: password
-  };
-  console.log(data);
-  fetch('/server/loginpage/login_page.php', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(data)
-
-  })
-    .then(response => response.json())
-    .then((data) => {
-      if (data.status === "error") {
-        console.error("Server Error:", data.Error);
-      }
-    })
-
-});*/
-
-
 function validateForm() {
 var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
