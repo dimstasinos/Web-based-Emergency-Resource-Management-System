b5f31cf14426c880695d0c8da2f9df500f0c6753
Dimitris Stasinos <staa.dista@yahoo.gr>
2023-12-28 11:59:40 +0200

    rescuer

---
+++ scripts/rescuer_map.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+  var map = L.map('map').setView([37.9838, 23.7275], 13);
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '© OpenStreetMap contributors'
+  }).addTo(map);
+
+
+  
+});
