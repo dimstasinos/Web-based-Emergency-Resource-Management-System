72e2cce837804d20b4555d4674ac5cd5e35171ab
VasilisPistiolas <83075825+VasilisPistiolas@users.noreply.github.com>
2024-01-24 19:16:47 +0200

    ok

--- html/citizen/citizen_requests.php
+++ html/citizen/citizen_requests.php
@@ -20,50 +20,56 @@ if (!isset($_SESSION["type"]) || $_SESSION["type"] != "citizen") {
 </head>
 
 <body>
-<div class="headerall">
+  <div class="headerall">
     <div id="pic"><img src="/images/favicon.png" alt="Icon" class="title-icon">
     </div>
     <div class="side_header">
       <div class="header">
         <h1 class="page-title">Αιτήματα</h1>
       </div>
-  <div class="sidebar">
-    <a href="/html/citizen/citizen_offers.php">Προσφορές</a>
-    <a href="/server/logout.php">Αποσύνδεση</a>
-  </div>
-</div>
-</div>
-  
-
-  <div>
-    <h5>Kατηγορίες</h5>
-    <label for="categories"></label>
-    <select id="categories"></select>
-  </div>
-  <div id="autocomlete">
-    <input type="text" id="search" placeholder="Items search..." autocomplete="off">
-    <ul id="results"></ul>
-  </div>
-  <div id="tables">
-    <div id=table_req>
-      <h2>Αποθήκη</h2>
-      <div class="table-data">
-      <table id="table_admin_request">
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>'Ονομα Προϊόντος</th>
-            <th>Κατηγορία</th>
-            <th>Λεπτομέρειες</th>
-            <th>Ποσότητα</th>
-            
-          </tr>
-        </thead>
-        <tbody id="itemsTable"></tbody>
-      </table>
+      <div class="sidebar">
+        <a href="/html/citizen/citizen_offers.php">Προσφορές</a>
+        <a href="/server/logout.php">Αποσύνδεση</a>
       </div>
     </div>
+  </div>
+
+  <div id="main_container">
+  <div>
+    <label>Kατηγορίες</label>
+  </div>
     <div>
+      <label for="categories"></label>
+      <select id="categories"></select>
+    </div>
+    <div id="autocomlete">
+      <input type="text" id="search" placeholder="Items search..." autocomplete="off">
+      <ul id="results"></ul>
+    </div>
+    <div id="tables">
+      <h2>Αποθήκη</h2>
+      <div id=table_req>
+
+        <table id="table_admin_request">
+        <div>
+          <thead>
+            
+              <tr>
+                <th>ID</th>
+                <th>'Ονομα Προϊόντος</th>
+                <th>Κατηγορία</th>
+                <th>Λεπτομέρειες</th>
+                <th>Ποσότητα</th>
+
+              </tr>
+        
+          </thead>
+          </div>
+          <tbody id="itemsTable"></tbody>
+        </table>
+      </div>
+    </div>
+    <div id="items_selected">
       <h3>Επιλεγμένα Προϊόντα </h3>
       <table id="tableSelected">
         <thead>
@@ -76,11 +82,13 @@ if (!isset($_SESSION["type"]) || $_SESSION["type"] != "citizen") {
         </thead>
         <tbody id="itemSelected"></tbody>
       </table>
+    </div>
+    <div>
       <button id="submitRequest">Καταχώρηση</button>
     </div>
-  </div>
-  <div>
-  <h4>Αιτήματα</h4>
+
+    <div id="request_t">
+      <h4>Αιτήματα</h4>
       <table id="table_request">
         <thead>
           <tr>
@@ -95,6 +103,8 @@ if (!isset($_SESSION["type"]) || $_SESSION["type"] != "citizen") {
         <tbody id="requests"></tbody>
       </table>
     </div>
+  </div>
+  </div>
 
   <script src="/scripts/citizen_requests.js"></script>
 </body>
