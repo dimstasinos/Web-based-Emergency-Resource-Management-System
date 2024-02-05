b8596f3abb354b62fe168237141f76354d7e768a
Dimitris Stasinos <staa.dista@yahoo.gr>
2024-02-05 13:55:22 +0200

    ok

--- sql/project.sql
+++ sql/project.sql
@@ -3,50 +3,61 @@ create table users (
   username VARCHAR(50) not null,
   password VARCHAR(50) not null,
   user_type enum ('citizen', 'rescuer', 'admin') NOT NULL,
-  primary key (user_id, username)
+  primary key (user_id,username)
 );
 
 create table admins (
-  admin_id int not null,
-  f_name VARCHAR(50) not null,
-  l_name varchar(50) not null,
-  CONSTRAINT ADMIN_TABLE FOREIGN KEY (admin_id) REFERENCES users(user_id) on update CASCADE on delete cascade
+admin_id int not null,
+f_name VARCHAR(50) not null,
+l_name varchar(50) not null,
+CONSTRAINT ADMIN_TABLE
+FOREIGN KEY (admin_id) REFERENCES users(user_id)
+on update CASCADE on delete cascade
 );
 
 CREATE TABLE citizen (
-  citizen_id INT not null,
-  f_name VARCHAR(50) not null,
-  l_name VARCHAR(50) not null,
-  phone_number VARCHAR(20) NOT NULL,
-  latitude float NOT NULL,
-  longitude float NOT NULL,
-  primary key (citizen_id, phone_number),
-  CONSTRAINT TABLE_USER FOREIGN KEY (citizen_id) REFERENCES users(user_id) on update CASCADE on delete cascade
+citizen_id INT not null,
+f_name VARCHAR(50) not null,
+l_name VARCHAR(50) not null,
+phone_number VARCHAR(20) NOT NULL,
+latitude  float NOT NULL,
+longitude float NOT NULL,
+primary key (citizen_id,phone_number),
+CONSTRAINT TABLE_USER
+FOREIGN KEY (citizen_id) REFERENCES users(user_id)
+on update CASCADE on delete cascade
 );
 
 create table vehicle(
-  vehicle_id INT auto_increment,
-  vehicle_username VARCHAR(50) not null,
-  latitude float NOT NULL,
-  longitude float NOT NULL,
-  primary key (vehicle_id, vehicle_username),
-  CONSTRAINT vehicle_user FOREIGN KEY (vehicle_id) REFERENCES users(user_id) on update CASCADE on delete cascade
+vehicle_id INT auto_increment,
+vehicle_username VARCHAR(50) not null,
+latitude float NOT NULL,
+longitude float NOT NULL,
+primary key (vehicle_id,vehicle_username),
+CONSTRAINT vehicle_user
+FOREIGN KEY (vehicle_id) REFERENCES users(user_id)
+on update CASCADE on delete cascade
 );
 
 create table rescuer (
-  rescuer_id int not null,
-  f_name VARCHAR(50) not null,
-  l_name VARCHAR(50) not null,
-  rescuer_vehicle_id int not null,
-  primary key(rescuer_id),
-  constraint RESQ_ID foreign key (rescuer_id) references users(user_id) on update CASCADE on delete cascade,
-  constraint RESQ_VEH foreign key (rescuer_vehicle_id) references vehicle(vehicle_id) on update CASCADE on delete cascade
+	rescuer_id int not null,
+    f_name VARCHAR(50) not null,
+	l_name VARCHAR(50) not null,
+	rescuer_vehicle_id int not null,
+    primary key(rescuer_id),
+    constraint RESQ_ID
+    foreign key (rescuer_id) references users(user_id)
+    on update CASCADE on delete cascade,
+    constraint RESQ_VEH
+    foreign key (rescuer_vehicle_id) references vehicle(vehicle_id)
+    on update CASCADE on delete cascade
 );
+
 
 create table item_category (
   category_id INT not null,
   category_name VARCHAR(50) not null,
-  primary key(category_id, category_name)
+  primary key(category_id,category_name)
 );
 
 create table items (
@@ -55,14 +66,18 @@ create table items (
   item_category INT not null,
   item_quantity int not null,
   primary key(item_id),
-  CONSTRAINT TABLE_CAT FOREIGN KEY (item_category) REFERENCES item_category(category_id) on update CASCADE on delete CASCADE
+  CONSTRAINT TABLE_CAT 
+  FOREIGN KEY (item_category) REFERENCES item_category(category_id)
+  on update CASCADE on delete CASCADE
 );
 
 create table item_details(
   item_detail_id int not null,
   item_detail_name VARCHAR(100),
   item_detail_value VARCHAR(100),
-  CONSTRAINT TABLE_DET FOREIGN KEY (item_detail_id) REFERENCES items(item_id) on update CASCADE on delete CASCADE
+  CONSTRAINT TABLE_DET
+  FOREIGN KEY (item_detail_id) REFERENCES items(item_id)
+  on update CASCADE on delete CASCADE
 );
 
 CREATE TABLE base(
@@ -79,11 +94,16 @@ CREATE TABLE citizen_requests (
   req_citizen_id INT not null,
   req_item_id INT not null,
   primary key (request_id),
-  constraint CITIZEN_REQ_VEH FOREIGN KEY (req_veh_id) REFERENCES vehicle(vehicle_id) on update CASCADE on delete CASCADE,
-  constraint CITIZEN_REQ_ID FOREIGN KEY (req_citizen_id) REFERENCES citizen(citizen_id) on update CASCADE on delete CASCADE,
-  constraint REQ_ITEM_ID FOREIGN KEY (req_item_id) REFERENCES items(item_id) on update CASCADE on delete CASCADE
+  constraint CITIZEN_REQ_VEH
+  FOREIGN KEY (req_veh_id) REFERENCES vehicle(vehicle_id)
+  on update CASCADE on delete CASCADE,
+  constraint CITIZEN_REQ_ID
+  FOREIGN KEY (req_citizen_id) REFERENCES citizen(citizen_id)
+  on update CASCADE on delete CASCADE,
+  constraint REQ_ITEM_ID
+  FOREIGN KEY (req_item_id) REFERENCES items(item_id)
+  on update CASCADE on delete CASCADE
 );
-
 create table citizen_requests_complete(
   request_id INT,
   submission_date DATETIME not null,
@@ -94,9 +114,15 @@ create table citizen_requests_complete(
   req_item_id INT not null,
   complete_date datetime,
   primary key (request_id),
-  constraint CITIZEN_REQ_VEH_CMP FOREIGN KEY (req_veh_id) REFERENCES vehicle(vehicle_id) on update CASCADE on delete CASCADE,
-  constraint CITIZEN_REQ_ID_CMP FOREIGN KEY (req_citizen_id) REFERENCES citizen(citizen_id) on update CASCADE on delete CASCADE,
-  constraint REQ_ITEM_ID_CMP FOREIGN KEY (req_item_id) REFERENCES items(item_id) on update CASCADE on delete CASCADE
+  constraint CITIZEN_REQ_VEH_CMP
+  FOREIGN KEY (req_veh_id) REFERENCES vehicle(vehicle_id)
+  on update CASCADE on delete CASCADE,
+  constraint CITIZEN_REQ_ID_CMP
+  FOREIGN KEY (req_citizen_id) REFERENCES citizen(citizen_id)
+  on update CASCADE on delete CASCADE,
+  constraint REQ_ITEM_ID_CMP
+  FOREIGN KEY (req_item_id) REFERENCES items(item_id)
+  on update CASCADE on delete CASCADE
 );
 
 CREATE TABLE citizen_offers (
@@ -107,8 +133,12 @@ CREATE TABLE citizen_offers (
   offer_citizen_id INT not null,
   announcement_id INT not null,
   PRIMARY KEY (offer_id),
-  constraint CITIZEN_VEH_ID FOREIGN KEY (offer_veh_id) REFERENCES vehicle(vehicle_id) on update CASCADE on delete CASCADE,
-  constraint CITIZEN_OFFER_ID FOREIGN KEY (offer_citizen_id) REFERENCES citizen(citizen_id) on update CASCADE on delete CASCADE
+  constraint CITIZEN_VEH_ID
+  FOREIGN KEY (offer_veh_id) REFERENCES vehicle(vehicle_id)
+  on update CASCADE on delete CASCADE,
+  constraint CITIZEN_OFFER_ID
+  FOREIGN KEY (offer_citizen_id) REFERENCES citizen(citizen_id)
+  on update CASCADE on delete CASCADE
 );
 
 CREATE TABLE citizen_offers_complete (
@@ -119,40 +149,55 @@ CREATE TABLE citizen_offers_complete (
   offer_citizen_id INT not null,
   complete_date datetime,
   PRIMARY KEY (offer_id),
-  constraint CITIZEN_VEH_ID_OFF FOREIGN KEY (offer_veh_id) REFERENCES vehicle(vehicle_id) on update CASCADE on delete CASCADE,
-  constraint CITIZEN_OFFER_ID_OFF FOREIGN KEY (offer_citizen_id) REFERENCES citizen(citizen_id) on update CASCADE on delete CASCADE
+  constraint CITIZEN_VEH_ID_OFF
+  FOREIGN KEY (offer_veh_id) REFERENCES vehicle(vehicle_id)
+  on update CASCADE on delete CASCADE,
+  constraint CITIZEN_OFFER_ID_OFF
+  FOREIGN KEY (offer_citizen_id) REFERENCES citizen(citizen_id)
+  on update CASCADE on delete CASCADE
 );
 
 create table vehicle_storage(
-  str_vehicle_id int not null,
-  str_item_id int not null,
-  str_quantity int not null,
-  primary key(str_vehicle_id, str_item_id),
-  CONSTRAINT STR_ID FOREIGN KEY (str_vehicle_id) REFERENCES vehicle(vehicle_id) on update CASCADE on delete cascade,
-  CONSTRAINT STR_ITEM FOREIGN KEY (str_item_id) REFERENCES items(item_id) on update CASCADE on delete cascade
+str_vehicle_id int not null,
+str_item_id int not null,
+str_quantity int not null,
+primary key(str_vehicle_id,str_item_id),
+CONSTRAINT STR_ID
+FOREIGN KEY (str_vehicle_id) REFERENCES vehicle(vehicle_id)
+on update CASCADE on delete cascade,
+CONSTRAINT STR_ITEM
+FOREIGN KEY (str_item_id) REFERENCES items(item_id)
+on update CASCADE on delete cascade
 );
 
 CREATE TABLE offer_items (
   offer_id_item int not null,
   item_id_offer int not null,
   quantity int not null,
-  primary key(offer_id_item, item_id_offer),
-  constraint OFFER_ID FOREIGN KEY (offer_id_item) REFERENCES citizen_offers(offer_id) on update CASCADE on delete CASCADE,
-  constraint ITEM_ID FOREIGN KEY (item_id_offer) REFERENCES items(item_id) on update CASCADE on delete CASCADE
+  primary key(offer_id_item,item_id_offer),
+  constraint OFFER_ID
+  FOREIGN KEY (offer_id_item) REFERENCES citizen_offers(offer_id)
+  on update CASCADE on delete CASCADE,
+  constraint ITEM_ID	
+  FOREIGN KEY (item_id_offer) REFERENCES items(item_id)
+  on update CASCADE on delete CASCADE
 );
 
 create table announcements(
-  announcement_id int auto_increment,
-  primary key (announcement_id)
+	announcement_id int auto_increment,
+    primary key (announcement_id)
 );
 
 create table announcement_items(
-  announcement_id int not null,
-  announcement_item_id int not null,
-  announcement_item_quantity int not null,
-  primary key(announcement_id, announcement_item_id),
-  constraint ANN_ID FOREIGN KEY (announcement_id) REFERENCES announcements(announcement_id),
-  constraint ANN_ITEM_ID FOREIGN KEY (announcement_item_id) REFERENCES items(item_id) on update CASCADE on delete CASCADE
+	announcement_id int not null,
+    announcement_item_id int not null,
+    announcement_item_quantity int not null,
+    primary key(announcement_id,announcement_item_id),
+    constraint ANN_ID	
+	FOREIGN KEY (announcement_id) REFERENCES announcements(announcement_id),
+    constraint ANN_ITEM_ID	
+	FOREIGN KEY (announcement_item_id) REFERENCES items(item_id)
+	on update CASCADE on delete CASCADE
 );
 
 INSERT INTO
@@ -178,7 +223,7 @@ VALUES
 INSERT INTO
   vehicle
 VALUES
-  (NULL, "oxima", 37.997674, 23.667426),
+  (NULL, "oxima_1", 37.997674, 23.667426),
   (NULL, "oxima_2", 38.024325, 23.789415),
   (
     NULL,
@@ -261,61 +306,54 @@ values
   (2, 125, 2),
   (2, 143, 6),
   (2, 114, 2),
-  (3, 29, 3),
-  (3, 115, 7);
+  (3, 29, 3);
 
 INSERT INTO
   citizen_requests
 VALUES
-  (NULL, now(), 5, NULL, 2, 4, 17),
-  (NULL, now(), 2, now(), 1, 5, 16),
-  (NULL, now(), 4, NULL, NULL, 4, 19),
-  (NULL, now(), 2, NULL, NULL, 4, 25),
-  (NULL, now(), 7, now(), 1, 5, 18),
-  (NULL, now(), 3, NULL, NULL, 5, 20);
+  (
+    NULL,
+    '2024-02-04 14:00:26',
+    5,
+    NULL,
+    NULL,
+    4,
+    20
+  ),
+  (NULL, '2024-02-05 16:20:00', 2, now(), 1, 5, 25),
+  (
+    NULL,
+    '2024-02-03 12:35:00',
+    4,
+    NULL,
+    NULL,
+    6,
+    125
+  ),
+  (NULL, '2024-02-04 17:00:00', 2, now(), 2, 4, 115);
 
 insert into
   citizen_offers
 values
-  (NULL, now(), NULL, 1, 4, 1),
-  (NULL, now(), NULL, 2, 5, 2);
-
-insert into
-  citizen_offers_complete
-values
+  (NULL, '2024-02-04 9:25:00', NULL, 3, 7, 4),
   (
+    NULL,
+    '2024-02-05 8:40:00',
+    '2024-02-05 15:25:00',
     1,
-    '2024-02-04 13:23:44',
-    '2024-02-10 10:00:00',
-    1,
-    4,
-    now()
-  );
-
-insert into
-  citizen_requests_complete
-values
-  (
-    1,
-    '2024-02-04 13:23:44',
-    5,
-    '2024-02-10 10:00:00',
-    1,
-    4,
-    18,
-    now()
-  );
+    8,
+    5
+  ),
+  (NULL, '2024-02-03 15:19:00', NULL, NULL, 2, 8, 6);
 
 insert into
   offer_items
 VALUES
-  (4, 20, 8),
-  (3, 25, 5);
+  (1, 115, 2),
+  (1, 125, 4),
+  (2, 20, 5),
+  (3, 26, 10);
 
-insert into
-  vehicle_storage
-values
-  (1, 18, 5);
 
 create index item_cat on items (item_category);
 
